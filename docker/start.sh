6ca5393a2927dc92f0d553daad51ea16cb768457
baraa-baba <baraababawork@gmail.com>
2026-04-25 15:13:59 +0300

    fix:stop seeding on nex deploy

--- docker/start.sh
+++ docker/start.sh
@@ -4,10 +4,7 @@ php-fpm &
 
 # Run migrations safely (no data loss)
 php artisan migrate --force
-
-# Seed ONLY if needed (careful!)
-php artisan db:seed --force
-php8.4 artisan db:seed --class=UserSeeder
+ 
 
 # Cache optimizations
 php artisan config:cache
